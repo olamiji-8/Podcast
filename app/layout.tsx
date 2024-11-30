84dc60c858729ce8b55c263ae7a57f009aaa79a3
olamiji-8 <olamijisotomi@gmail.com>
2024-11-30 07:44:40 +0100

    creating my schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+                <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
